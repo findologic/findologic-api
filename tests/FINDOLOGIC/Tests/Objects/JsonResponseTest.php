c0d891eb8bb3dd8624eaf2b9b193ccafadfe9c69
TheKeymaster <d.brader@findologic.com>
2018-09-02 23:25:15 +0200

    Add tests for json suggestion response

--- tests/FINDOLOGIC/Tests/Objects/JsonResponseTest.php
+++ tests/FINDOLOGIC/Tests/Objects/JsonResponseTest.php
@@ -20,13 +20,210 @@ class JsonResponseTest extends TestCase
         return new JsonResponse($realResponseData);
     }
 
-    public function testResponseWillReturnValidData()
+    public function testResponseWillReturnExpectedLabels()
     {
+        $expectedLabels = [
+            'siemens',
+            'sama',
+            'Schulthess',
+            'Siemens',
+            'Sale%',
+            'Staubsauger',
+            'Miele Dampfgarer mit Mikrowelle DGM 6600, 60 cm, Edelstahl, SensorTronic, MultiSteam, 1000 W',
+            'Bosch Geschirrspüler SPV40E40EU, Vollintegrierbar, 45 cm, 9 Massgedecke, A+',
+            'Bosch Geschirrspüler SBA88TD16E PerfectDry XXL Geschirrspüler 60cm mit Home Connect Vollintegrierbar',
+            'Bosch Geschirrspüler SBE46MX03E SuperSilence XXL Geschirrspüler 60cm Vollintegrierbar mit VarioScharnier',
+        ];
         $response = $this->getRealResponseData();
 
-        // TODO: Add tests
-        //foreach ($response->getSuggestions() as $suggestion) {
-        //    $this->assertEquals('', $suggestion->getLabel());
-        //}
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedLabels[$count], $suggestion->getLabel());
+            $count++;
+        }
+    }
+
+    public function testResponseWillReturnExpectedBlocks()
+    {
+        $expectedBlocks = [
+            'suggest',
+            'suggest',
+            'vendor',
+            'vendor',
+            'cat',
+            'cat',
+            'product',
+            'product',
+            'product',
+            'product',
+        ];
+        $response = $this->getRealResponseData();
+
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedBlocks[$count], $suggestion->getBlock());
+            $count++;
+        }
+    }
+
+    public function testResponseWillReturnExpectedFrequencies()
+    {
+        $expectedFrequencies = [
+            '617',
+            '552',
+            '0',
+            '0',
+            '0',
+            '0',
+            '1',
+            '1',
+            '1',
+            '1',
+        ];
+        $response = $this->getRealResponseData();
+
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedFrequencies[$count], $suggestion->getFrequency());
+            $count++;
+        }
+    }
+
+    public function testResponseWillReturnExpectedImageUrls()
+    {
+        $expectedImageUrls = [
+            '',
+            '',
+            'https://www.blubbergurken.io/images/gallery/Findologic/vendor/schulthess.png',
+            'https://www.blubbergurken.io/images/gallery/Findologic/vendor/siemens.png',
+            '',
+            '',
+            'https://www.blubbergurken.io/m4dlk92njtak/item/images/10248/full/20000122510-000-00-20000122510.jpg',
+            'https://www.blubbergurken.io/m4dlk92njtak/item/images/10598/full/MCSA01744491-G5678-SPV40E40EU-1182799-def-0.jpg',
+            'https://www.blubbergurken.io/m4dlk92njtak/item/images/11563/full/MCSA01757436-G6327-SBA88TD16E-1194162-def-0.jpg',
+            'https://www.blubbergurken.io/m4dlk92njtak/item/images/11564/full/MCSA01732370-G5009-SBE46MX03E-1173959-def-0.jpg',
+        ];
+        $response = $this->getRealResponseData();
+
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedImageUrls[$count], $suggestion->getImageUrl());
+            $count++;
+        }
+    }
+
+    public function testResponseWillReturnExpectedPrice()
+    {
+        $expectedPrice = [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            2305.75,
+            678.00,
+            1355.65,
+            866.00,
+        ];
+        $response = $this->getRealResponseData();
+
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedPrice[$count], $suggestion->getPrice());
+            $count++;
+        }
+    }
+
+    public function testResponseWillReturnExpectedIdentifier()
+    {
+        $expectedIdentifier = [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '10248',
+            '10598',
+            '11563',
+            '11564',
+        ];
+        $response = $this->getRealResponseData();
+
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedIdentifier[$count], $suggestion->getIdentifier());
+            $count++;
+        }
+    }
+
+    public function testResponseWillReturnExpectedBasePrice()
+    {
+        $expectedBasePrice = [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            1355.65,
+            '',
+        ];
+        $response = $this->getRealResponseData();
+
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedBasePrice[$count], $suggestion->getBasePrice());
+            $count++;
+        }
+    }
+
+    public function testResponseWillReturnExpectedBasePriceUnit()
+    {
+        $expectedBasePriceUnit = [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '€',
+            '',
+        ];
+        $response = $this->getRealResponseData();
+
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedBasePriceUnit[$count], $suggestion->getBasePriceUnit());
+            $count++;
+        }
+    }
+
+    public function testResponseWillReturnExpectedUrl()
+    {
+        $expectedUrl = [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            'http://www.blubbergurken.io/de/kochen-und-backen/miele-dampfgarer-mit-mikrowelle-dgm-6600-edelstahl/a-10248/',
+            'http://www.blubbergurken.io/de/bosch-geschirrspueler-spv40e40eu/a-10598/',
+            'http://www.blubbergurken.io/de/bosch-geschirrspueler-sba88td16e/a-11563/',
+            'http://www.blubbergurken.io/de/bosch-geschirrspueler-sbe46mx03e/a-11564/',
+        ];
+        $response = $this->getRealResponseData();
+
+        $count = 0;
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals($expectedUrl[$count], $suggestion->getUrl());
+            $count++;
+        }
     }
 }
