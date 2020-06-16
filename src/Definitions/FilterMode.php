09ff7fed3d730b54ff85a7cd6d2a67d4e67739b3
Huzaifa Mustafa <24492269+zaifastafa@users.noreply.github.com>
2020-06-16 17:28:51 +0500

    SW-417-add-filter-mode-definition (#72)

---
+++ src/Definitions/FilterMode.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FINDOLOGIC\Api\Definitions;
+
+class FilterMode extends Definition
+{
+    const
+        SINGLE = 'single',
+        MULTIPLE = 'multiple',
+        MULTISELECT = 'multiselect';
+}
